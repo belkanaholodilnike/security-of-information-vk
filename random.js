8243e5e796ab3580be36b892ac21af5004ba49f2
Morgen Matvey <melges.morgen@gmail.com>
2014-10-19 11:49:52 +0400

    Write some function for random generator.

--- random.js
+++ random.js
@@ -1,3 +1,22 @@
-function random () {
-  return "747%+%";
+var mouseCoordBuffer = new Array();
+
+/**
+ * Listener which should be called when mouse coordinates is changed.
+ */
+function mouseMoved(docElement) {
+  mouseCoordBuffer.push(docElement.pageX);
+  mouseCoordBuffer.push(docElement.pageY);
 }
+
+/**
+ * Function returns random string with specifed number of characters
+ * @param num number of characters which geerated string must have.
+ * @return random string or null if we couldn't generate string with
+ * specifed length.
+ */
+function random(num) {
+  // TODO: Function body
+}
+
+
+document.addEventListener("mousemove", mouseMoved);
