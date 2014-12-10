0b22063d200f2646f65ff459bbac32f178ea9dbf
Morgen Matvey <melges.morgen@gmail.com>
2014-12-11 02:43:22 +0300

    Fix error with precision. Add automatic precision calculation based on key size.

--- common.js
+++ common.js
@@ -5,9 +5,6 @@ svkm.crypto = function () {};
 svkm.crypto.math = function () {};
 svkm.crypto.elgamal = function() {};
 
-
-Decimal.config({ precision: 840 });
-
 var IS_ENABLED_KEY = "is_enabled";
 svkm.basic.isSecureFormEnabled = function() {
   if (localStorage.getItem(IS_ENABLED_KEY) === null) {
